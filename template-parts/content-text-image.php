37daac405a38e38e1c87f55be3acf76e25729669
lmoffat <leonmfft@gmail.com>
2021-03-07 09:08:35 +0100

    Mise en page responsive texte image

--- template-parts/content-text-image.php
+++ template-parts/content-text-image.php
@@ -9,16 +9,17 @@
                 $section_image = $row['text_img-image']['sizes']['large'];
                 $section_desc = $row['text_img-content'];
                 $has_button = $row['text_img-button'];
-                $button_url = $row['text_img-url_button']; ?>
-                <div class="desc-img__container col-12">
-                    <div class="desc-img__content col-12 col-md-5">
+                $button_url = $row['text_img-url_button'];
+                ?>
+                <div class="desc-img__container">
+                    <div class="desc-img__content col-md-5">
                         <h2 class="title_h2"><?php echo $section_title; ?></h2>
                         <p><?php echo $section_desc; ?></p>
                         <?php if ( $has_button ) : ?>
                             <a class="desc-img__button button" href="<?php echo $button_url ?>">Acheter</a>
                         <?php endif; ?>
                     </div>
-                    <div class="desc-img__image col-12 col-md-7"><img src="<?php echo $section_image; ?>"></div>
+                    <div class="desc-img__image  col-md-7"><img src="<?php echo $section_image; ?>"></div>
                 </div>
                 <?php else : ?>
                     <h2>Ne marche pas</h2>
